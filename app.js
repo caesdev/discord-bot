b84f39732190971a916297eb1b5ed0a282715cec
Carlos Escudero <caescudero97@gmail.com>
2022-12-17 09:00:00 -0500

    remove logs

--- app.js
+++ app.js
@@ -4,17 +4,15 @@ const { RateLimiter } = require('discord.js-rate-limiter')
 
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-let rateLimiter = new RateLimiter(1, 60000);
+let timeLimiter = new RateLimiter(1, 60000);
 
 client.on('ready', () => {
     client.user.setActivity(`perseguir offliners`);
-    console.log('bot has started ✅');
 });
 
 client.on('messageCreate', async (message) => {
     if (message.content === '!poder') {
-        let limiter = rateLimiter.take(message.author.id);
-        console.log(limiter);
+        let limiter = timeLimiter.take(message.author.id);
         if (limiter) {
             message.reply({
                 content: `${message.author.username} no seas intenso, espera un minuto`
