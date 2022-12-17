aea7b42648eac7d9962cc3d72cdb15656be8efc2
Carlos Escudero <caescudero97@gmail.com>
2022-12-17 07:59:58 -0500

    add timeLimit for use command

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const { getPower } = require('./functions/getPower');
+const { RateLimiter } = require('discord.js-rate-limiter')
 
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
+let rateLimiter = new RateLimiter(1, 60000);
 
 client.on('ready', () => {
     client.user.setActivity(`perseguir offliners`);
@@ -12,9 +13,17 @@ client.on('ready', () => {
 
 client.on('messageCreate', async (message) => {
     if (message.content === '!poder') {
-        message.reply({
-            content: getPower()
-        })
+        let limiter = rateLimiter.take(message.author.id);
+        console.log(limiter);
+        if (limiter) {
+            message.reply({
+                content: `${message.author.username} no seas intenso, espera un minuto`
+            })
+        } else {
+            message.reply({
+                content: getPower()
+            })
+        }
     }
 });
 
